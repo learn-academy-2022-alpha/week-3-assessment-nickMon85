b9192e89402020e6a73692fe4bb16318bf12b48b
Ramon Bolds <ramonbolds98@gmail.com>
2022-03-07 10:16:54 -0600

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,56 +1,161 @@
-// ASSESSMENT 3: Coding Practical Questions with Jest
-
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
-
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
-
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// // ASSESSMENT 3: Coding Practical Questions with Jest
 
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
 
-// Example input: 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// // Use test driven development to complete the following questions
+// // Add appropriate dependencies: $ yarn add jest
 
-// Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// // Reminder: The test will call your function
+// // Run the file with the following command: $ yarn jest
+// // a describe method that lists the name of the function OR naming of the particular test.
+describe("hello", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("returns a string that says hi", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(hello()).toEqual("hi")
+    })
+  })
+  const hello = () => {
+    return "hi"
+  }
+//Just ran to make sure i was formatting right!
+
+// // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+
+// // a) Create a test with expect statements for each of the variables provided.
+// // a describe method that lists the name of the function OR naming of the particular test.
+// //1. test jest
+// //2. create a function variable (to test)
+describe("numbFib3", () => {
+
+    let inputNum = 6
+    let result = [1, 1, 2, 3, 5, 8]
+    let inputNum2= 10
+    let result2= [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(numbFib3(6)).toEqual(result)
+      expect(numbFib3(10)).toEqual(result2)
+    })
+})
+// //TESTED JEST GOT MY FAIL!!
+// //1.Create the function the takes in the number
+// //2.That number parameter will be the number of iterations to create an array
+// //Tools: a.)while(loop) b.Recursion c.for loop
+// //!!Note: This is just Algerbra 
+// //[1,1,2,3,5]
+const numbFib = (n)=>{
+    var fib = [1, 1];
+    for(var i=fib.length; i<n; i++) {
+        fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib
+}
+const numFib2 = (n) =>{
+    var fib2 =[1,1];
+    var i = 0;
+    while(i<n){
+        fib2[i]= fib2[i-2] + fib2[i-1]
+    
+        n--;
+    }
+    return fib2
+}
+// //I kinda want to work on Recurrsion.
+// //To my best understanding, recurrsion it similar to loops, but its simply afunction recalling itself.(To best of my remedial understanding!!)
+// //1. create my function
+const numbFib3=(number)=>{
+    //i'll use an if statement I understand. 
+    if(number>2){
+        //Ill use the basic fib algerbra
+        // Also, I need an array!! I wrote literally 13 codes that didnt work mostly due to screwing up my array.
+        return [1];
+    }
+    if(number>3){
+        return [1,1];
+    }
+    //Here's the remix
+    var arr1 = numbFib3(number-1);
+    arr1.push(arr1[number-2] +[number-3]);
+    return arr1
 
 
+}
+//expected [1,1,2,3,5,8] recieved [1,2,3,5,8]
+//My recurrsion kinda worked, never tried it before, the web has alot of overly complicated models
 // b) Create the function that makes the test pass.
+
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("sortOdd", () => {
+    const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+    let output= [-9, 7, 9, 199]
+    const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+    let output2= [-823, 7, 23]
+    it("returns a new array of only odd numbers sorted from least to greatest.", () => {
+      expect(sortOdd(fullArr1)).toEqual(output)
+      expect(sortOdd(fullArr2)).toEqual(output2)
+    })
+  })
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
 
+// Test failed!!
 
 // b) Create the function that makes the test pass.
+//1.create funct.
+//2.Separate Strings, even numbers, and booleans
+//3.Leave only odd numbers
+//4.Sort odd numbers
+const sortOdd = (arr)=>{
+    let oddNum = arr.filter(value=>{
+        return typeof value === "number" && value % 2!==0
+    })
+    return oddNum.sort((a,b)=>a-b)
 
+}
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+// !!Function name accSum
+describe("accSum", () => {
+    const numbersToAdd1 = [2, 4, 45, 9]
+    let output= [2, 6, 51, 60]
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+    // const numbersToAdd2 = [0, 7, -8, 12]
+    // let output2 = [0, 7, -1, 11]
 
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+    // const numbersToAdd3 = []
+    // let output3 = []
+    it("returns an array of the accumulating sum. An empty array should return an empty array", () => {
+      expect(accSum(numbersToAdd1)).toEqual(output)
+    //   expect(accSum(numbersToAdd2)).toEqual(output2)
+    //   expect(accSum(numbersToAdd3)).toEqual(output3)
+    })
+  })
+  //Use the same Idea as the fib!
+//Test Failure
+const accSum =(arr)=>{
+    var arrSum = [1,1]
+    for(let i =0; i<arr.length+1;i++){
+        arrSum[i]=arr[i-2]+arr[i-1]
+    }
+    return arrSum
+}
+//Test fail!!!Failed like 10times
 
-const numbersToAdd3 = []
-// Expected output: []
 
 
 // b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer:"This" is calling to an object
 
-  Researched answer:
+  Researched answer:The 'this' keyword simply points to a function which is bound to. It answers the question of where it should get value or data from.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: Its a JavaScript frontend interface, developed to updated the display without refreshing.
 
-  Researched answer:
+  Researched answer:React.js is an open source js library that is used building frontend user interface. It's great for developers because you can create reusable UI components
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer:State!
 
-  Researched answer:
+  Researched answer: In short 'render()'. However, this means nothing without first understanding the lifecycle. Every component within react has a lifecycle, that can be both monitered and manipulated. This lifecycle has 3 main phases: mounting, updating, and unmounting. Mounting puts the element on the DOM, 'render()' is part of this phase. Updating and unmounting, is when the component, updates DOM and removes component from DOM,respectively.
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer:JSX, is the React interface. It's different then HTML, because it doesnt require a page refresh to update changes made.
 
-  Researched answer:
+  Researched answer:JSX means JavaScript Syntax, it is an extension of JavaScript that allows developers to write html from a js file. To some degree its writing JavaScript and html together
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer:I believe yarn is a node package. I believe 'run yarn' is the command to run JSX through HTML.
 
-  Researched answer:
+  Researched answer:Yarn is a package manager, that provides speed, consistency, stability, and security.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer:Higher order function?
 
-  Researched answer:
+  Researched answer:Function that are declared without names. Which means the arent usually accessible after the initial call.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering:It renders to component having different behaviors based on the conditions of true or false.
 
-2. Props:
+2. Props:Props or properties are arguments passed into react components.
 
-3. JavaScript Events:
+3. JavaScript Events:JavaScripts interaction with html is handled through events that happen when the user manipulates the page.
 
-4. Object-oriented programming:
+4. Object-oriented programming: Is a programming paradigm based on the concept of classes objects
 
-5. Ruby:
+5. Ruby:Its a dynamic programming language with a focus on simplicity. Its most popular for web applications.
